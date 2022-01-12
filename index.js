1efe79dde661b2ef47dfd198eaee9df80938eab2
Amanda <amandamaiken@berkeley.edu>
2022-01-12 08:59:41 -0800

    commit tree 577007d12504adde8900cf822f793cc92af72b2c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (driverArray) => { 
+    return driverArray.slice(0,2);
+}
+
+const returnLastTwoDrivers = (driverArray) => {
+    return driverArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return (fare) => {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, returnDriversFunc) {
+    return returnDriversFunc (drivers);
+}
